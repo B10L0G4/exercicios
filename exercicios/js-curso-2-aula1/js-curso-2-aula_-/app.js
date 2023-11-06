84f5828264c8b52fef1182a8ecfea38ea24cb093
B10l0g4 <vandyyh@gmail.com>
2023-11-06 16:07:36 -0300

    commit linha

---
+++ exercicios/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Número Secreto o Jogo';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1e 10';
+
+function verificarChute() {
+    console.log('Clique de botao')
+}
