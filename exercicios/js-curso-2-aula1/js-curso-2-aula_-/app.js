82534c56ccb8543934ecbd3558cfe1fe36e244b2
B10l0g4 <vandyyh@gmail.com>
2023-11-07 15:08:16 -0300

    projeto final com speech

--- exercicios/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ exercicios/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,18 +1,52 @@
+let listaEscolhidos = []
+let limite = 10
 let secret = geradorDeNumeor()
+let chances = 1
 
-function verificaTag (tag , texto) {
+function verificaTag (tag , texto ) {
     let campo = document.querySelector(tag)
     campo.innerHTML = texto
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2})
 }
 
 function verificarChute() {
-    console.log(secret)
+    let chute = document.querySelector('input').value
+
+    if (chute == secret){
+        let chance = chances > 1 ? ' chances' :' chance';
+        verificaTag('h1','Você acertou em '+ chances + chance)
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    }else 
+        if (chute > secret){
+        verificaTag('p','O número é menor ')
+        }else{
+            verificaTag('p','O número é maior')
+    }
+    chances++
+    clear()
 }
 function geradorDeNumeor(){
-    return parseInt(Math.random(10) * 10 + 1 )
-
+    let numEscolhido = parseInt(Math.random() * limite + 1 )
+        return numEscolhido;
+    }
+function clear(){
+    chute = document.querySelector('input')
+    chute.value = ''
+}
+function exibirMensagens() {
+    verificaTag('h1', 'Número Secreto o Jogo')
+    verificaTag('p', 'Escolha um núnero de 1 a 10')
 }
 
-verificaTag('h1', 'Número Secreto o Jogo')
-verificaTag('p', 'Escolha um núnero de 1 a 10')
+exibirMensagens()
+
+function reiniciarJogo(){
+    geradorDeNumeor()
+    clear()
+    chances = 1
+    exibirMensagens()
+    document.getElementById('reiniciar').setAttribute('disabled',true)
+}
+
+
 
---
+++ exercicios/js-curso-2-desafio_1/func.js
@@ -0,0 +1,28 @@
+function world(){
+    console.log('Olá Mundo ')
+}
+function nameW(name){
+    console.log('Olá '+ name)
+}
+function dobro(num){
+    console.log(parseInt(num * 2))
+}
+function media(num,num1,num2){
+    console.log(parseInt(num + num1 + num2)/3)
+}
+function maior(num, num1){
+    let numMax = Math.max(num,num1)
+    console.log(numMax)
+}
+function mesmo(num){
+    console.log(num * num)
+}
+
+world()
+nameW('Vanessa')
+dobro(15)
+media(5,5,6)
+maior(12,1254)
+mesmo(2)
+
+
